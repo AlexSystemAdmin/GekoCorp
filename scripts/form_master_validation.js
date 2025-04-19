dd45813bfa7302dc49b1462e560a4925d2d6fb39
Alexandr Kuznetsov <alexkuzne7soff@yandex.ru>
2025-04-20 01:57:05 +0300

    reason-of-appeal

--- scripts/form_master_validation.js
+++ scripts/form_master_validation.js
@@ -120,7 +120,7 @@ form.addEventListener('submit', (event) => {
 });
 
 form.addEventListener('input', (event) => {
-    //onInput(event.target);
+    onInput(event.target);
 })
 
 inputName.addEventListener('keypress', (event) => {
